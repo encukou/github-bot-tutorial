8bf4e2183b88acb8b32758b58a7025d2aaa6064b
Petr Viktorin <encukou@gmail.com>
2019-03-24 14:02:24 +0100

    Heroku bot app

---
+++ github_bot/__main__.py
@@ -0,0 +1,8 @@
+from octomachinery.app.server.runner import run as run_app
+
+if __name__ == "__main__":
+    run_app(
+        name='Comment Reactor - PyCon SK tutorial (@encukou)',
+        version='1.0.0',
+        url='https://github.com/apps/comment-reactor-pycon-sk-tutorial',
+    )
